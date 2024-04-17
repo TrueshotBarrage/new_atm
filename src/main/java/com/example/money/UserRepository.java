da4f91dbfdc1fd66aaf09346f87ebbcedd0f3af8
David Kim <David.Kim5@ey.com>
2024-04-17 14:40:49 -0400

    Initial commit of boilerplate API

---
+++ src/main/java/com/example/money/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.money;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<AtmUser, Long>, CrudRepository<AtmUser, Long> {
+    List<AtmUser> findByLastName(@Param("name") String name);
+}
